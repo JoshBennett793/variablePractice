fbd646f20d0f060fc8568f573957132a3df7fddc
Josh Bennett <joshbennett793@yahoo.com>
2023-04-18 14:18:04 -0500

    Add three boolean variables

--- variables.js
+++ variables.js
@@ -1,7 +1,11 @@
-var javascript = "JavaScript is cool!";
+var javascript = 'JavaScript is cool!';
 var ruby = "Ruby is probably cool too, but I don't have much experience with it!";
-var coding = "Coding is awesome!";
+var coding = 'Coding is awesome!';
 
 var age = 26;
 var currentYear = 2023;
 var birthYear = 1996;
+
+var isTuringStudent = true;
+var isMinnesotaResident = true;
+var isGoodAtCoding = false;
