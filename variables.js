6ae626f6303c28996011b8ad48f2a4c2dd4e20b7
Josh Bennett <joshbennett793@yahoo.com>
2023-04-18 14:16:01 -0500

    Add three number variables

--- variables.js
+++ variables.js
@@ -1,3 +1,7 @@
 var javascript = "JavaScript is cool!";
 var ruby = "Ruby is probably cool too, but I don't have much experience with it!";
 var coding = "Coding is awesome!";
+
+var age = 26;
+var currentYear = 2023;
+var birthYear = 1996;
